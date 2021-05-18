38a3f014baeeba2fc7e7743f9331cc5b838d8d19
ziwang0411 <ziwang0411@gmail.com>
2021-05-17 23:09:37 -0700

    added models and some services

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
@@ -0,0 +1,11 @@
+package com.udacity.jwdnd.course1.cloudstorage.mapper;
+
+import com.udacity.jwdnd.course1.cloudstorage.model.Note;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Options;
+
+public interface NoteMapper {
+    @Insert("INSERT INTO NOTES (notetitle, notedescription, userid) VALUES(#{noteTitle}, #{noteDescription}, #{userId})")
+    @Options(useGeneratedKeys = true, keyProperty = "userId")
+    int insert(Note note);
+}
